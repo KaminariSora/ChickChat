19e18e862ad6cd9b2ecfb041ee4713efa7f206d8
Pongsakorn Weraburudpongsatorn <ping_pongsakorn@hotmail.com>
2023-11-10 15:49:11 +0700

    .

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,7 @@ module.exports = {
         fontFamily: {
             // sans: ['Graphik', 'sans-serif'],
             // serif: ['Merriweather', 'serif'],
+            cherry: ['Cherry Bomb One'],
         },
     },
     plugins: [
